19de685f6bcf5abf6db1b20961b23851490ff6fe
Abhishek Majumdar <abhi.sam32@gmail.com>
2022-03-09 00:00:51 +0530

    Added solution for longest word problem (LC)

---
+++ src/main/com/leetcode/dsa/arrays/LongestWordInASentence.java
@@ -0,0 +1,32 @@
+package main.com.leetcode.dsa.arrays;
+
+//Solved - not sure if optimal solution O(n)
+public class LongestWordInASentence {
+
+    /**
+     * REGEX for cleaning : "[^\\w\\s]"
+     *
+     *
+     * @param sen
+     * @return
+     */
+    public String LongestWord(String sen) {
+        String[] listOfWords;
+        String largestWord = "";
+        String cleanedSen = sen.replaceAll("[^\\w\\s]","");
+
+        listOfWords = cleanedSen.split(" ");
+        for(String word : listOfWords){
+            if(word.length() > largestWord.length())
+                largestWord = word;
+        }
+        return largestWord;
+    }
+
+    public static void main(String[] args) {
+        LongestWordInASentence obj = new LongestWordInASentence();
+        String sen = "I love dogs avery much";
+        System.out.println(obj.LongestWord(sen));
+    }
+
+}
