60a73a5e0c60aa49739cd572875ca59aaed5d6fd
Abhishek Majumdar <abhi.sam32@gmail.com>
2022-03-18 17:00:52 +0530

    Added solutions to more recursion problems

--- src/main/com/leetcode/dsa/algorithm/Recursion.java
+++ src/main/com/leetcode/dsa/algorithm/Recursion.java
@@ -1,9 +1,13 @@
 package main.com.leetcode.dsa.algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Recursion {
 
     public long computeFactorial(long num){
-        long factorial = 1;
+        long factorial;
         if(num == 1){
             return 1;
         }
@@ -21,13 +25,64 @@ public class Recursion {
         return factorial;
     }
 
-    //public
+    public int nthFibonacciNumber(int num){
+        int nthFibonacciNumber = 0;
+        if(num == 0)
+            return 0;
+        if(num == 1 || num == 2)
+            return 1;
+
+        nthFibonacciNumber = nthFibonacciNumber(num-1) + nthFibonacciNumber(num-2);
+        return nthFibonacciNumber;
+    }
+
+    public int nthFibonacciNumberIterativeUsingList(int num){
+        int nthFibonacciNumber = 0;
+        if(num == 0)
+            return 0;
+        if(num == 1 || num == 2)
+            return 1;
+
+        List<Integer> fibonacciNumbers = new ArrayList<>();
+        fibonacciNumbers.add(0);
+        fibonacciNumbers.add(1);
+        fibonacciNumbers.add(1);
+
+        for(int i = 3; i <= num; i++){
+            fibonacciNumbers.add(fibonacciNumbers.get(i-1) + fibonacciNumbers.get(i-2));
+        }
+        nthFibonacciNumber = fibonacciNumbers.get(fibonacciNumbers.size() -1);
+        return nthFibonacciNumber;
+    }
+
+    public int nthFibonacciNumberIterative(int num){
+        int nthFibonacciNumber = 0;
+        if(num == 0)
+            return 0;
+        if(num == 1 || num == 2)
+            return 1;
+
+        int last = 1;
+        int secondLast = 1;
+
+        for(int i = 3; i <= num; i++)
+        {
+            nthFibonacciNumber = last + secondLast;
+            secondLast = last;
+            last = nthFibonacciNumber;
+        }
+        return nthFibonacciNumber;
+    }
 
     public static void main(String[] args) {
         Recursion obj = new Recursion();
 
         System.out.println(obj.computeFactorial(10));
         System.out.println(obj.computeFactorialIterative(10));
+
+        System.out.println(obj.nthFibonacciNumberIterativeUsingList(11));
+        System.out.println(obj.nthFibonacciNumberIterative(11));
+        System.out.println(obj.nthFibonacciNumber(11));
     }
 
 }
